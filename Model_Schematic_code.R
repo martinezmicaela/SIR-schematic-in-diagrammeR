26c4643cae689c2a31c6cabf46da1044b5f4b6e0
Micaela <micaela.martinezbakker@gmail.com>
2015-03-09 13:55:32 -0400

    I have completed the example code

---
+++ Model_Schematic_code.R
@@ -0,0 +1,54 @@
+library('DiagrammeR')
+
+# The neato engine uses random seeds to do diagram orientation,
+# I have set the seed to start = 6906 when I call the function
+# by changing the seed you will get a different layout
+myplot <- "
+digraph neato {
+
+  # several 'node' statements
+	node [shape = circle,
+   		style=filled,
+        color = '#B2A389'] // sets as circles
+  		Births;
+
+	node [shape = circle,
+   		style=filled,
+        color = '#F2E3B6'] // sets as circles
+  		SB1; SB2; SB3; SB4; SB5; SB6 ; SO;
+
+	node [shape = circle,
+   		style=filled,
+       color = '#D55D45'] // sets as circles
+  		IB; IO;
+
+	node [shape = oval,
+   		style=filled,
+        color = '#BCDAF2'] // sets as circles
+  		Immune;
+
+	node [shape = oval,
+   		style=filled,
+        color = '#026773',fontcolor=white] // sets as circles
+  		Symptomatic;
+
+	node [shape = oval,
+   		style=filled,
+        color = black,fontcolor=white] // sets as circles
+  		Reported;
+
+  # several 'edge' statements
+    edge [arrowhead=vee]
+	    Births->SB1 SB1->SB2 SB2->SB3 SB3->SB4 SB4->SB5 SB5->SB6 SB6->SO
+	    SB1->IB SB2->IB SB3->IB SB4->IB SB5->IB SB6->IB
+	    SO->IO
+	    IO->Immune IB->Immune
+
+    edge [arrowhead=vee,style=dashed]
+	    IO-> Symptomatic Symptomatic->Reported
+
+  # a 'graph' statement
+  graph [overlap = true, fontsize = 10,mindist=0.4]
+}
+"
+grViz(myplot,engine='neato',options= list(start = 6906))
